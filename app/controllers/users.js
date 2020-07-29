eabc444ab956c09a21bbcebc013e1e9fc899f8b6
Ravikumar <ravikumar.t@westagilelabs.com>
2020-07-29 20:53:56 +0530

    password hashing

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,5 +1,6 @@
 const User = require("../models/users");
 const response = require('../config/response');
+const md5 = require('md5');
 //const UserRoles = require("../models/userRoles");
 
 const create = async (req, res) => {
@@ -7,6 +8,7 @@ const create = async (req, res) => {
     if(await User.findOne({ email: req.body.email })){
       response.failed("Already registered with the email " + req.body.email, res);
     } else {
+      req.body.password = md5(req.body.password);
       const user = new User(req.body);
       await user.save();
       response.success("User created succesfully", res);
@@ -26,7 +28,7 @@ const list = async (req, res) => {
 };
 
 const checkUser = async (email, password) => {
-  const user = await User.findOne({ email: email, password: password, active: 1 });
+  const user = await User.findOne({ email: email, password: md5(password), active: 1 });
   return user;
 };
 
