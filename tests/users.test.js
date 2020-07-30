1055dd60480137be4163edf854f396102a56477f
Ravikumar <ravikumar.t@westagilelabs.com>
2020-07-30 17:44:04 +0530

    user test case 2.0

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,24 +1,40 @@
 const request = require('supertest');
 const app = require('../app');
 
-test("Should not create the user", async () =>{
-    await request(app)
-      .post('/users')
-      .send({
-            "firstName": "Ravi",
-            "lastName": "T",
-            "email": "ravi1@wal.com",
-            "password": "kumar123",
-            "phone": "96184602219",
-            "userType": 2,
-            "active": 1
-        })
-      .expect(400)
+
+test("Should create the user: Checking with new User", async () =>{
+  await request(app)
+    .post('/users')
+    .send({
+          "firstName": "Ravi",
+          "lastName": "T",
+          "email": "ravikumar.t123@wal.com",
+          "password": "ravi123",
+          "phone": "9618460221",
+          "userType": 2,
+          "active": 1
+      })
+    .expect(200)
 });
 
 
+test("Should not create the user: Checking with existing user", async () =>{
+  await request(app)
+    .post('/users')
+    .send({
+          "firstName": "Ravi",
+          "lastName": "T",
+          "email": "ravi1@wal.com",
+          "password": "kumar123",
+          "phone": "96184602219",
+          "userType": 2,
+          "active": 1
+      })
+    .expect(400)
+});
 
-test("Should not return users", async () =>{
+
+test("Should not return users: Only the admin can get users", async () =>{
     await request(app)
       .get('/users')
       .expect(400)
